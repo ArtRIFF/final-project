2908091c6bafdb359837393754df234f879138a8
Artem Panasuik <artriff.panasiuk@gmail.com>
2022-11-08 23:53:04 +0200

    create global scss structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-
+import './global_scss/global_style.scss'
 // import { BrowserRouter } from 'react-router-dom';
 // import ViewProvider from './context';
 // import store from './store/store';
@@ -10,7 +10,7 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-        <App />
+        <App className='global'/>
 );
 
 
