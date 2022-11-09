232150c94fb5a227906758a2172327c4e8d75682
Artem Panasuik <artriff.panasiuk@gmail.com>
2022-11-09 17:46:00 +0200

    add items to sections

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
     <>
     <h1 className='title'>Start Project</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nihil est eveniet labore illum, repudiandae aperiam assumenda possimus earum, laborum velit voluptate autem fugiat provident. Suscipit architecto consequuntur voluptatibus sint.</p>
-   <OurAdvantageSection/>
     </>
   );
 }
