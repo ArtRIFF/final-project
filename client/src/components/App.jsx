802085260c98f7501e6e2bf295d2f4ee2326e779
Artem Panasuik <artriff.panasiuk@gmail.com>
2022-11-09 17:30:01 +0200

    create component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import OurAdvantageSection from './OurAdvantageSection/OurAdvantageSection';
 
 const App = () => {
   return (
     <>
     <h1 className='title'>Start Project</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nihil est eveniet labore illum, repudiandae aperiam assumenda possimus earum, laborum velit voluptate autem fugiat provident. Suscipit architecto consequuntur voluptatibus sint.</p>
+   <OurAdvantageSection/>
     </>
   );
 }
