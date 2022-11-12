3bdd53b06219aa750c073fae1d85b31c7c2d773a
Andrii Ostin <ostin.andrew1@gmail.com>
2022-11-13 00:56:36 +0200

    news section created, no adaptive and button (next step)

---
+++ client/src/components/NewsSection/NewsSection.jsx
@@ -0,0 +1,50 @@
+import "./NewsSection.scss";
+import ButtonHeader from "../Button/ButtonHeader(ButtonCollection)/ButtonHeader"
+
+const NewsSection = () => {
+  return (
+    <section className="news-section">
+      <div className="news-section-container">
+        <img
+          src="img/news-section/body.png"
+          alt="sorry, we've lost this pic but trying to bring it back"
+          className="news-section__body"
+        />
+        <img
+          src="img/news-section/hand.png"
+          alt="sorry, we've lost this pic but trying to bring it back"
+          className="news-section__hand"
+        />
+        <h2 className="news-section__title">New Collection</h2>
+        <p className="news-section__summary">
+          <div className="news-section__summary_margin">
+            <i className="news-section__summary_red">Breeze</i> - a collection
+            of variability. That we adapt to circumstances just like water
+            adapts to wind currents.
+            <i className="news-section__summary_red">
+              Adaptation is an important human skill.
+            </i>
+          </div>
+          <div className="news-section__summary_margin">
+            Sometimes the mood is calm, like a sea surface, and sometimes it is
+            stormy. But as soon as the storm subsides, thoughts
+            <i className="news-section__summary_red">fly to the shores</i> in a
+            breeze - to where it is cozy and carefree. Where a ring of yellow
+            gold with mother-of-pearl makes sense.
+          </div>
+          <div className="news-section__summary_margin">
+            After all, the
+            <i className="news-section__summary_red">
+              details become noticeable
+            </i>
+            when significant problems subside. And we dream that thanks to this
+            jewelry story you will feel the ease of life.
+          </div>
+        </p>
+        {/* <ButtonHeader /> */}
+      </div>
+    </section>
+  );
+};
+
+export default NewsSection;
