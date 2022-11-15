bd591ef9e1f123f6f9938ce4eed56b33d004e98b
Missollie <96629551+Missollie@users.noreply.github.com>
2022-11-15 13:11:21 +0200

    headerHero (#16)
    Co-authored-by: OLha Kuzmenchuk <olhakuzmenchuk@gmail.com>

---
+++ client/src/components/HeroSection/BtnSeeMore/BtnSeeMore.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss'
+
+const BtnSeeMore = () => {
+    return (
+        <section className='btn-seemore'>
+              <p className='btn-seemore__btn'>See More</p>
+        </section>
+    )
+}
+export default BtnSeeMore;
