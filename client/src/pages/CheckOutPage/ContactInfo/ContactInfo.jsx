2dc133b904a33367b22137d96fe668ed65073ce9
Lana Mysa <84589656+LanaMysa@users.noreply.github.com>
2022-11-25 16:03:15 +0300

    Galery (#40)

--- client/src/pages/CheckOutPage/ContactInfo/ContactInfo.jsx
+++ client/src/pages/CheckOutPage/ContactInfo/ContactInfo.jsx
@@ -1,23 +1,74 @@
-import React from 'react';
-import './style.scss';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import "./ContactInfo.scss";
+import { NavLink } from "react-router-dom";
 
 const ContactInfoPage = () => {
+  const [email, setEmail] = useState("");
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Invalid email");
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true);
+        break;
+    }
+  };
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Invalid email");
+    } else setEmailError(" ");
+  };
+
   return (
-    <section className='contact-info-section'>
-      <div className='container'>
-        <p className='contact-info-section__title'>1. Contact Information</p>
+    <div>
+      <h2 className="contact-info-section__way ">
+        Shop /<span> Buy now </span>
+      </h2>
+      <section className="contact-info-section">
+        <div className="contact-info-section__wrap">
+          <p className="contact-info-section__title">1. Contact Information</p>
 
-        <div className='contact-info-section__info'>
-          <form action="" className='contact-info-section__infoEmail'>
-            <label htmlFor="#" className='contact-info-section__email'>Email Address</label>
-            <input type="text" className='contact-info-section__text' placeholder='elonamail@gmail' /></form>
+          <div className="contact-info-section__info">
+            <form
+              autoComplete="off"
+              action=""
+              className="contact-info-section__infoEmail"
+            >
+              <label htmlFor="#" className="contact-info-section__email">
+                Email Address
+              </label>
+
+              <input
+                onBlur={(e) => blurHandler(e)}
+                onChange={(e) => emailHandler(e)}
+                name="email"
+                value={email}
+                type="text"
+                className="contact-info-section__text"
+                placeholder="elonamail@gmail"
+              />
+
+              {emailDirty && emailError && (
+                <div className="contact-info-section__error-message">
+                  {emailError}
+                </div>
+              )}
+            </form>
+          </div>
+          <p className="contact-info-section__titleLogin">
+            Already have an account?{" "}
+            <NavLink to="/login" className="contact-info-section__login">
+              Login
+            </NavLink>
+          </p>
         </div>
-        <p className='contact-info-section__titleLogin'>Already have an account? <NavLink to="/login" className='contact-info-section__login'>Login</NavLink> </p>
-
-      </div>
-    </section>
-    
-  )
-}
+      </section>
+    </div>
+  );
+};
 export default ContactInfoPage;
