1ff54aaaa3337e902f08cb7044b45c01939ebe0f
Kirill Zubenko <zubenko92@gmail.com>
2022-12-08 14:26:28 +0200

    subscribe section interaction with backend

--- client/src/components/SubscribeSection/SubscribeSection.jsx
+++ client/src/components/SubscribeSection/SubscribeSection.jsx
@@ -10,10 +10,11 @@ const SubscribeSection = () => {
   const [subscriberEmail, setSubscriberEmail] = useState("");
 
   const handleSubmit = () => {
+    /*Fixme лист на пошту поки що не приходить, в перспективі можна зверстати тематичну newsletter, що буде надходити підписнику */
     const newSubscriber = {
       email: subscriberEmail,
       letterSubject: "Welcome subscribe letter from Jewelry Store",
-      letterHtml: "<!DOCTYPE html><html lang='en'> <head> <meta charset='UTF-8' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /> <meta http-equiv='X-UA-Compatible' content='ie=edge' /> <title>Document</title> <style> td { padding: 20px 50px; background-color: yellow; color: blueviolet; font-size: 20px; } </style> </head> <body> <table> <tr> <td>Test1</td> <td>Test2</td> <td>Test3</td> </tr> <tr> <td>Test1.1</td> <td>Test2.1</td> <td>Test3.1</td> </tr> </table> </body></html>"
+      letterHtml: "<!DOCTYPE html><html lang='en'> <head> <meta charset='UTF-8' /> <meta name='viewport' content='width=device-width, initial-scale=1.0' /> <meta http-equiv='X-UA-Compatible' content='ie=edge' /> <title>Document</title> <style> div { padding: 20px 50px; background-color: yellow; color: blueviolet; font-size: 20px; } </style> </head> <body> <div><h2>You have successfully subscribed to our newsletter</h2><p>We will keep you updated on discount and new collection information!</p></div></body></html>"
     };
 
     sendRequest(`${API}subscribers`, 'POST',
