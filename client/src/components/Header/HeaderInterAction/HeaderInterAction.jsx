a405b9adbd3c5319df7c575a4e2caf6636b70ccb
Andrii Myroshnychenko <miroha199366@gmail.com>
2022-12-24 20:54:39 +0200

    fixed header, button

--- client/src/components/Header/HeaderInterAction/HeaderInterAction.jsx
+++ client/src/components/Header/HeaderInterAction/HeaderInterAction.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import HeaderSearch from "./HeaderSearch/HeaderSearch";
-import HeaderFavorites from "./HeaderFavorites/HeaderFavorites";
+import Button from "../../Button/ButtonAll/ButtonAll";
+import { NavLink } from "react-router-dom";
 import HeaderRegistration from "./HeaderRegistration/HeaderRegistration";
 import HeaderCart from "./HeaderCart/HeaderCart";
 
 import "./HeaderInterAction.scss";
 
-class HeaderInterAction extends React.Component {
-  render() {
+const HeaderInterAction = () => {
     return(
-    <div className="header-interaction__wrap">
-      <HeaderSearch />
-      <HeaderFavorites />
-      <HeaderCart />
-      <HeaderRegistration />
-    </div>); 
+      <div className="header-interaction__wrap">
+        <div className="header-search">
+          <input type="text" placeholder="Search" className="nav__search"/>
+          <Button text='Search' className='section__btn-seacrh' id='section__btn' />
+        </div>
+        <div className="header__icon">
+          <NavLink to="/favourite" className="header-favorites__icon">
+            <img
+              className="header-favorites__icon"
+              src="img/header-icon/Favouritesicon.svg"
+              alt="favorites jewelry">
+            </img>
+          </NavLink>
+          <HeaderCart/>
+          <HeaderRegistration/>
+        </div>
+      </div>
+    ); 
   }
-}
 
 export default HeaderInterAction;
