f6dc81c9debd36a012014237b9844e2f8e2f1815
Andrii Myroshnychenko <miroha199366@gmail.com>
2022-12-26 16:04:20 +0200

    add search, fixed icon cart, add breadcrums in ProductCard

--- client/src/components/Header/HeaderInterAction/HeaderInterAction.jsx
+++ client/src/components/Header/HeaderInterAction/HeaderInterAction.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import Button from "../../Button/ButtonAll/ButtonAll";
 import { NavLink } from "react-router-dom";
 import HeaderRegistration from "./HeaderRegistration/HeaderRegistration";
 import HeaderCart from "./HeaderCart/HeaderCart";
+import Search from "./Search/Search";
 
 import "./HeaderInterAction.scss";
 
 const HeaderInterAction = () => {
     return(
       <div className="header-interaction__wrap">
-        <div className="header-search">
-          <input type="text" placeholder="Search" className="nav__search"/>
-          <Button text='Search' className='section__btn-seacrh' id='section__btn' />
-        </div>
+        <Search/>
         <div className="header__icon">
           <NavLink to="/favourite" className="header-favorites__icon">
             <img
