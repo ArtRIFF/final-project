9b2fda54ad5e86f016ae2f3b3aea2d31458d5945
Elena Akisheva <eakisheva@gmail.com>
2023-01-09 16:36:04 +0100

    old price and discount, adaptive in shopping cart

--- client/src/pages/ShoppingCart/ShoppingCart.jsx
+++ client/src/pages/ShoppingCart/ShoppingCart.jsx
@@ -16,45 +16,47 @@ const ShoppingCart = () => {
         localStorage.setItem("inCart", JSON.stringify(inCart));
       }, [inCart]);
 
-    const totalPrice = inCart.reduce((acc, cur) => acc + cur.price*cur.quantity, 0)
+    const totalPrice = inCart.reduce((acc, cur) => acc + cur.price*cur.quantity, 0);
+    const totalOldPrice = inCart.reduce((acc, cur) => acc + cur.price*100*cur.quantity/(100 - cur.discount), 0);
+    const discount = Math.round((totalOldPrice-totalPrice)*100/totalOldPrice);
 
     return (
         <section className="cart-section container">
             <div className="container">
-            <div className="breadcrumbs__cart">
-                <Breadcrumbs />
+                <div className="breadcrumbs__cart">
+                    <Breadcrumbs />
+                </div>
+                    {!!inCart.length && <> 
+                    <h3 className="cart-section__title">Order Summary</h3>
+                        <div className="cart-section__wrapper">
+                            <div className="cart-section__products">                    
+                            {inCart.map((card, cardID) => {
+                                return( 
+                                    <ItemInCart
+                                        key={cardID}
+                                        card={card}/>
+                                )
+                                })}
+                            </div>
+                            <div className="cart-section__summary-wrapper">
+                                <div className="cart-section__summary">
+                                    <p >Shipping</p>
+                                    <p className="cart-section__summary-shipping">free</p>
+                                    <p>Discount</p>
+                                    <p>{discount}%</p>
+                                    <p>Tax</p>
+                                    <p>-</p>
+                                    <p>Total</p>
+                                    <p className="cart-section__summary-total">&#8372; {totalPrice}</p>    
+                                </div>
+                                <Link to="/checkout"> 
+                                    <Button className={"section__btn-continue"} text={"Continue to checkout"}/>
+                                </Link>
+                            </div>
+                        </div>
+                </>}
+                {!inCart.length && <h3 className="cart-section__title">Your cart is empty</h3>}
             </div>
-            {!!inCart.length && <> 
-            <h3 className="cart-section__title">Order Summary</h3>
-                <div className="cart-section__wrapper">
-                    <div className="cart-section__products">                    
-                    {inCart.map((card, cardID) => {
-                        return( 
-                            <ItemInCart
-                                key={cardID}
-                                card={card}/>
-                    )
-                    })}
-                </div>
-                <div className="cart-section__summary-wrapper">
-                    <div className="cart-section__summary">
-                        <p >Shipping</p>
-                        <p className="cart-section__summary-shipping">free</p>
-                        <p>Discount</p>
-                        <p>10%</p>
-                        <p>Tax</p>
-                        <p>-</p>
-                        <p>Total</p>
-                        <p className="cart-section__summary-total">&#8372; {totalPrice}</p>    
-                    </div>
-                    <Link to="/checkout"> 
-                        <Button className={"section__btn-header"} text={"Continue to checkout"}/>
-                    </Link>
-                </div>
-           </div>
-           </>}
-           {!inCart.length && <h3 className="cart-section__title">Your cart is empty</h3>}
-        </div>
         </section>
      );
  };
