712db690d4365d3785d9b8aec311b8d8c894da15
Artem <34552610+ArtRIFF@users.noreply.github.com>
2023-01-09 18:51:18 +0200

    Fix ProductCard errors  (#109)
    * add few tests
    
    * start test category filter
    
    * resolve problem marge
    
    * fix style cards container
    
    * fixed category filter
    
    * fix category filter
    
    * add filter tests
    
    * aresolve conflict
    
    * fix searchpage and ourProductionPage
    
    * fix errors in productcard
    
    * fix catalogSectionPage style
    
    Co-authored-by: Artem Panasuik <artriff.panasiuk@gmail.com>

--- client/src/pages/ProductCard/ProductPrice/ProductPrice.jsx
+++ client/src/pages/ProductCard/ProductPrice/ProductPrice.jsx
@@ -69,8 +69,8 @@ const ProductPrice = (props) => {
           >
             <option value="not choose"></option>
             {size !== undefined &&
-              size.split(",").map((el) => {
-                return <option value={el}>{el}</option>;
+              size.split(",").map((el, index) => {
+                return <option key={index} value={el}>{el}</option>;
               })}
           </select>
           <div className="select-arrow"></div>
