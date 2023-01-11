40edc413fbeb0af6d4b97a9efa9d8dc87d683453
Ostin-Andrii <72520161+Ostin-Andrii@users.noreply.github.com>
2023-01-11 14:49:53 +0200

    additional test (#113)

--- client/src/tests/CatalogSectionPage.test.js
+++ client/src/tests/CatalogSectionPage.test.js
@@ -1,18 +1,51 @@
-import CatalogSectionPage from '../pages/CatalogSectionPage/CatalogSectionPage';
-import '@testing-library/jest-dom';
+import CatalogSectionPage from "../pages/CatalogSectionPage/CatalogSectionPage";
+import "@testing-library/jest-dom";
 import { render, cleanup, screen, fireEvent } from "@testing-library/react";
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 import { Provider } from "react-redux";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import store from "../store/store";
+
+import LoadingSpinner from "../pages/CatalogSectionPage/components/LoadingSpinner/LoadingSpinner";
+
 
 afterEach(cleanup);
 
 it("renders with snapshot", () => {
-  const catalogSectionPage = renderer.create( <Provider store={store}>
-    <BrowserRouter>
-    <CatalogSectionPage/>
-    </BrowserRouter>
-    </Provider>).toJSON()
+  const catalogSectionPage = renderer
+    .create(
+      <Provider store={store}>
+        <BrowserRouter>
+          <CatalogSectionPage />
+        </BrowserRouter>
+      </Provider>
+    )
+    .toJSON();
   expect(catalogSectionPage).toMatchSnapshot();
+});
+
+describe("CatalogSectionPage works correctly", () => {
+  it("CatalogSectionPage renders", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <CatalogSectionPage />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  });
+
+  const dataArray = [];
+  it("CatalogSectionPage with no items", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <CatalogSectionPage alreadyFilteredArray = {dataArray}/>
+        </BrowserRouter>
+      </Provider>
+    );
+     expect(screen.queryByText("Next", "Prev")).not.toBeInTheDocument();
+     expect((<LoadingSpinner />)).toBeDefined();
+  });
 });
