25de99bb281fc5a5ed2800a4ddd25d2c0662b505
Ostin-Andrii <72520161+Ostin-Andrii@users.noreply.github.com>
2023-01-30 17:45:39 +0200

    Catalog section (#154)
    * new Pagination logic and Spinner implementation
    
    * CatalogSectionPage changes
    
    * CatalogSectionPage changes
    
    * CatalogSectionPage changes

--- client/src/store/products/productsSlice.js
+++ client/src/store/products/productsSlice.js
@@ -3,49 +3,77 @@ import { getCards } from "../../API/cardsAPI";
 
 export const fetchOutlet = createAsyncThunk(
   "product/fetchByOutlet",
-  async (thunkAPI)=> {
-    const response = await getCards()
-    return response.filter(element => element.statusProduct === "OUTLET")
-  })
+  async (thunkAPI) => {
+    const response = await getCards();
+    return response.filter((element) => element.statusProduct === "OUTLET");
+  }
+);
 export const fetchAllCollectionProduct = createAsyncThunk(
   "product/fetchByAllProduct",
-  async (thunkAPI)=> {
-    const response = await getCards()
-    return response
-  })
+  async (thunkAPI) => {
+    const response = await getCards();
+    return response;
+  }
+);
 export const fetchNewCollectionProduct = createAsyncThunk(
   "product/fetchByNewCollection",
-  async (thunkAPI)=> {
-    const response = await getCards()
-    return response.filter((element) => element.statusProduct === "NEW")
-  })      
+  async (thunkAPI) => {
+    const response = await getCards();
+    return response.filter((element) => element.statusProduct === "NEW");
+  }
+);
 export const fetchBestsellers = createAsyncThunk(
   "product/fetchByBestsellers",
-  async (thunkAPI)=> {
-    const response = await getCards()
-    return response.filter((element) => element.statusProduct === "BESTSELLER")
-  })      
+  async (thunkAPI) => {
+    const response = await getCards();
+    return response.filter((element) => element.statusProduct === "BESTSELLER");
+  }
+);
 
 const initialState = {
-    newCollectionProduct: [],
-    allCollectionProduct: [],
-    bestsellers: [],
-    outlet: []
-}
+  isLoading: false,
+  newCollectionProduct: [],
+  allCollectionProduct: [],
+  bestsellers: [],
+  outlet: [],
+};
 
-const productsSlice = createSlice ({
-    name: "products",
-    initialState,
-    extraReducers: (builder) => {
-      builder.addCase(fetchOutlet.fulfilled, (state, { payload }) => {
-        state.outlet = payload});
-      builder.addCase(fetchAllCollectionProduct.fulfilled, (state, { payload }) => {
-        state.allCollectionProduct = payload});
-      builder.addCase(fetchNewCollectionProduct.fulfilled, (state, { payload }) => {
-        state.newCollectionProduct = payload}); 
-      builder.addCase(fetchBestsellers.fulfilled, (state, { payload }) => {
-        state.bestsellers = payload});
-    }
-})
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchOutlet.fulfilled, (state, { payload }) => {
+      state.outlet = payload;
+    });
+    builder.addCase(
+      fetchAllCollectionProduct.fulfilled,
+      (state, { payload }) => {
+        state.allCollectionProduct = payload;
+      }
+    );
+    builder.addCase(
+      fetchNewCollectionProduct.fulfilled,
+      (state, { payload }) => {
+        state.newCollectionProduct = payload;
+      }
+    );
+    builder.addCase(fetchBestsellers.fulfilled, (state, { payload }) => {
+      state.bestsellers = payload;
+    });
+    builder.addMatcher(
+      (action) => action.type.includes("/pending"),
+      (state) => {
+        state.isLoading = true;
+      }
+    );
+    builder.addMatcher(
+      (action) =>
+        action.type.includes("/rejected") || action.type.includes("/fulfilled"),
+      (state) => {
+        state.isLoading = false;
+      }
+    );
+  },
+});
 
 export default productsSlice.reducer;
